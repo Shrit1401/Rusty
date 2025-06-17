8d7feb0a0d50cab6f360cba1c8ffda23b320b5db
Shrit Shrivastava <67160842+Shrit1401@users.noreply.github.com>
2025-06-17 16:26:33 +0530

    learned about variables, func, loops'

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 fn main() {
-    let axx = "shrit \n";
+    // let name = String::from("shrit");
+    // let char1 = name.chars().nth(0);
 
-    print!("{}", axx);
+    // // match char1 {
+    // //     Some(c) => println!("{}", c),
+    // //     None => println!("No character found"),
+    // // }
+
+    // println!("{}", char1.unwrap());
+
+    let sent = String::from("my name is Shrit");
+    let first_word = get_first_word(sent);
+
+    println!("{}", first_word);
+}
+
+fn get_first_word(sentence: String) -> String {
+    let mut ans = String::new();
+
+    for word in sentence.chars() {
+        if word == ' ' {
+            break;
+        }
+        ans.push(word);
+    }
+
+    return ans;
 }
