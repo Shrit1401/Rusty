8bc2884da012cc567f2862b811aed2a8d23b2447
Shrit Shrivastava <67160842+Shrit1401@users.noreply.github.com>
2025-06-17 15:54:51 +0530

    rephrasing the code

--- target/debug/deps/rusty-3f5ec8fd74a74fe2.d
+++
@@ -1,5 +0,0 @@
-/Users/shritaake/Desktop/Code/solana/hs-learn/rusty/target/debug/deps/librusty-3f5ec8fd74a74fe2.rmeta: src/main.rs
-
-/Users/shritaake/Desktop/Code/solana/hs-learn/rusty/target/debug/deps/rusty-3f5ec8fd74a74fe2.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/rusty-c6e514c5278ca8a1.d
+++
@@ -1,5 +0,0 @@
-/Users/shritaake/Desktop/Code/solana/hs-learn/rusty/target/debug/deps/librusty-c6e514c5278ca8a1.rmeta: src/main.rs
-
-/Users/shritaake/Desktop/Code/solana/hs-learn/rusty/target/debug/deps/rusty-c6e514c5278ca8a1.d: src/main.rs
-
-src/main.rs:
